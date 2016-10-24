3367e248f70793a219d2ad2d38a39cd58afbfcba
Michael Hall <m.hall@kainos.com>
2016-10-24 13:35:21 +0100

    Feature/apvs 343/inital web node and landing page (#1)
    Initial addition of web node and a basic landing page
    Most content taken and edited from external web
    Added new README for internal web
    Added .gitignore file
    Added package.json
    Added docker file exposing port 3001 instead of 3000 like external
    Added app files and tests for initial internal site landing page
    Includes basic e2e tests

---
+++ app/app.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const nunjucks = require('express-nunjucks')
+const path = require('path')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const compression = require('compression')
+const routes = require('./routes/routes')
+const log = require('./services/log')
+const onFinished = require('on-finished')
+
+var app = express()
+
+// Use gzip compression - remove if possible via reverse proxy/Azure gateway.
+app.use(compression())
+
+// Set security headers.
+app.use(helmet())
+
+var packageJson = require('../package.json')
+var developmentMode = app.get('env') === 'development'
+var releaseVersion = packageJson.version
+var serviceName = 'Assisted Prison Visit Service'
+
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'views'))
+
+nunjucks(app, {
+  watch: developmentMode,
+  noCache: developmentMode
+})
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_template')))
+app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_frontend_toolkit')))
+app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'images', 'favicon.ico')))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Send assetPath to all views.
+app.use(function (req, res, next) {
+  res.locals.asset_path = '/public/'
+  next()
+})
+
+// Add variables that are available in all views.
+app.use(function (req, res, next) {
+  res.locals.serviceName = serviceName
+  res.locals.releaseVersion = 'v' + releaseVersion
+  next()
+})
+
+// Log each HTML request and it's response.
+app.use(function (req, res, next) {
+  // Log response started.
+  log.info({ request: req }, 'Route Started.')
+
+  // Log response finished.
+  onFinished(res, function () {
+    log.info({ response: res }, 'Route Complete.')
+  })
+
+  next()
+})
+
+// Build the router to route all HTTP requests and pass to the routes file for route configuration.
+var router = express.Router()
+routes(router)
+app.use('/', router)
+
+// catch 404 and forward to error handler.
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  res.status(404)
+  next(err)
+})
+
+// Development error handler.
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  if (err.status === 404) {
+    res.render('includes/error-404')
+  } else {
+    res.render('includes/error', {
+      error: developmentMode ? err : {}
+    })
+  }
+})
+
+module.exports = app
---
+++ app/services/log.js
@@ -0,0 +1,48 @@
+const bunyan = require('bunyan')
+const PrettyStream = require('bunyan-prettystream')
+const logsPath = process.env.LOGGING_PATH || 'logs/internal-web.log'
+
+// Stream to handle pretty printing of Bunyan logs to stdout.
+var prettyStream = new PrettyStream()
+prettyStream.pipe(process.stdout)
+
+// Create a base logger for the application.
+var log = bunyan.createLogger({
+  name: 'internal-web',
+  streams: [],
+  serializers: {
+    'request': requestSerializer,
+    'response': responseSerializer
+  }
+})
+
+// Add console Stream.
+log.addStream({
+  level: 'DEBUG',
+  stream: prettyStream
+})
+
+// Add file stream.
+log.addStream({
+  type: 'rotating-file',
+  level: 'DEBUG',
+  path: logsPath,
+  period: '1d',
+  count: 7
+})
+
+function requestSerializer (request) {
+  return {
+    url: request.url,
+    method: request.method,
+    params: request.params
+  }
+}
+
+function responseSerializer (response) {
+  return {
+    statusCode: response.statusCode
+  }
+}
+
+module.exports = log
