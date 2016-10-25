a03311e05c509e59e7c62a3eefd11f4dc867ce0b
Michael Hall <michaelha@square.local>
2016-10-25 12:06:41 +0100

    Added database call to get so index is populated with real data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,30 +1,17 @@
+const claims = require('../services/data/get-claims-by-status')
+const moment = require('moment')
+
 module.exports = function (router) {
   router.get('/', function (req, res) {
-    return res.render('index', {
-      title: 'APVS index',
-      claims: [
-        {
-          Reference: '1234567',
-          FirstName: 'Bill',
-          LastName: 'Bob',
-          DateCreated: '12-12-2012',
-          ClaimID: '123'
-        },
-        {
-          Reference: '9876543',
-          FirstName: 'Jill',
-          LastName: 'Nill',
-          DateCreated: '10-10-2010',
-          ClaimID: '456'
-        },
-        {
-          Reference: '18759308',
-          FirstName: 'Blobby',
-          LastName: 'Blob',
-          DateCreated: '11-11-2011',
-          ClaimID: '789'
-        }
-      ]
-    })
+    data = claims.get('New')
+      .then(function (data) {
+        data.forEach(function (claim) {
+          claim.DateSubmitted = moment(claim.DateSubmitted).format('DD-MM-YYYY HH:MM')
+        })
+        return res.render('index', {
+          title: 'APVS index',
+          claims: data
+        })
+      })
   })
 }
---
+++ app/services/data/get-claims-by-status.js
@@ -0,0 +1,13 @@
+const config = require('../../../knexfile').intweb
+const knex = require('knex')(config)
+
+module.exports.get = function (status) {
+  return knex('Claim')
+    .join('Eligibility', 'Claim.EligibilityId', '=', 'Eligibility.EligibilityId')
+    .join('Visitor', 'Eligibility.EligibilityId', '=', 'Visitor.EligibilityId')
+    .where('Claim.Status', status)
+    .select('Eligibility.Reference', 'Visitor.FirstName', 'Visitor.LastName', 'Claim.DateSubmitted', 'Claim.ClaimId')
+    .then(function (claims) {
+      return claims
+    })
+}
