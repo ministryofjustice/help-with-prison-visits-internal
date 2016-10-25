5ab3169cad9c9c4f9ed4646cb01658f3322f32c7
Michael Hall <michaelha@square.local>
2016-10-25 16:50:24 +0100

    Moved functionality of test to helper class for future tests

--- test/integration/services/data/test-get-claims-by-status.js
+++ test/integration/services/data/test-get-claims-by-status.js
@@ -1,7 +1,6 @@
 var expect = require('chai').expect
-var config = require('../../../../knexfile').migrations
-var knex = require('knex')(config)
 var moment = require('moment')
+var databaseHelper = require('../../../helpers/database-setup-for-tests')
 
 var claims = require('../../../../app/services/data/get-claims-by-status')
 var reference = 'V123456'
@@ -15,69 +14,13 @@ describe('services/data/get-claims-by-status', function () {
   describe('get', function (done) {
     before(function (done) {
       DATE = moment().toDate()
-      knex('IntSchema.Eligibility')
-        .insert({
-          Reference: reference,
-          DateCreated: DATE,
-          DateSubmitted: DATE,
-          Status: 'TEST'
-        })
-        .returning('EligibilityId')
-        .then(function (result) {
-          eligibilityId = result[0]
-          return knex('IntSchema.Prisoner')
-            .returning('PrisonerId')
-            .insert({
-              EligibilityId: eligibilityId,
-              FirstName: 'TestFirst',
-              LastName: 'TestLast',
-              DateOfBirth: DATE,
-              PrisonNumber: 'A123456',
-              NameOfPrison: 'Test'
-            })
-            .then(function (result) {
-              prisonerId = result[0]
-              return prisonerId
-            })
-        }).then(function () {
-          return knex('IntSchema.Visitor')
-          .returning('VisitorId')
-          .insert({
-            EligibilityId: eligibilityId,
-            Title: 'Mr',
-            FirstName: 'John',
-            LastName: 'Smith',
-            NationalInsuranceNumber: 'QQ123456c',
-            HouseNumberAndStreet: '1 Test Road',
-            Town: '1 Test Road',
-            County: 'Durham',
-            PostCode: 'bT111BT',
-            Country: 'England',
-            EmailAddress: 'test@test.com',
-            PhoneNumber: '07911111111',
-            DateOfBirth: DATE,
-            Relationship: 'partner',
-            JourneyAssistance: 'no',
-            RequireBenefitUpload: 0
-          })
-          .then(function (result) {
-            visitorId = result[0]
-            return prisonerId
-          })
-        }).then(function () {
-          return knex('IntSchema.Claim')
-          .returning('ClaimId')
-          .insert({
-            EligibilityId: eligibilityId,
-            DateOfJourney: DATE,
-            DateCreated: DATE,
-            DateSubmitted: DATE,
-            Status: 'TEST'
-          })
-        }).then(function (result) {
-          claimId = result[0]
-          done()
-        })
+      databaseHelper.setup(reference, DATE).then(function (ids) {
+        claimId = ids.claimId
+        eligibilityId = ids.eligibilityId
+        prisonerId = ids.prisonerId
+        visitorId = ids.visitorId
+        done()
+      })
     })
 
     it('should get 5 columns from 3 tables', function (done) {
@@ -97,17 +40,8 @@ describe('services/data/get-claims-by-status', function () {
     })
 
     after(function (done) {
-      console.log('after')
       // Clean up
-      knex('IntSchema.Claim').where('ClaimId', claimId).del().then(function () {
-        knex('IntSchema.Visitor').where('VisitorId', visitorId).del().then(function () {
-          knex('IntSchema.Prisoner').where('PrisonerId', prisonerId).del().then(function () {
-            knex('IntSchema.Eligibility').where('EligibilityId', eligibilityId).del().then(function () {
-              done()
-            })
-          })
-        })
-      })
+      databaseHelper.delete(claimId, eligibilityId, visitorId, prisonerId)
       done()
     })
   })
