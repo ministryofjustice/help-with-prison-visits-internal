02018c854966cef681c2beca62830045379dfa51
Michael Hall <michaelha@square.local>
2016-10-27 12:33:18 +0100

    Removed temp data as it was distracting

--- app/routes/claim/view-claim.js
+++ app/routes/claim/view-claim.js
@@ -9,38 +9,7 @@ module.exports = function (router) {
         data.DateOfBirth = moment(data.DateOfBirth).format('DD-MM-YYYY')
         return res.render('./claim/view-claim', {
           title: 'APVS Claim',
-          Claim: data,
-          temp: {
-            Prison: 'Featherstone',
-            PrisonerNumber: '00000001',
-            PrisonerFirstName: 'Bob',
-            PrisonerLastName: 'Bob',
-            Expense: [
-              {
-                TransportType: 'Taxi',
-                From: 'Lisburn Road',
-                To: 'Belfast Central',
-                Cost: '8',
-                Status: 'Approved'
-              },
-              {
-                TransportType: 'Train',
-                From: 'Belfast',
-                To: 'Newry',
-                Cost: '12',
-                IsReturn: 'True',
-                Status: ''
-
-              },
-              {
-                ExpenseType: 'Light Refreshment',
-                Cost: '5',
-                Description: 'Lunch',
-                Status: 'Approved'
-              }
-            ],
-            Total: '25'
-          }
+          Claim: data
         })
       })
   })
