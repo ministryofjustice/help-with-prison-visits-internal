9341fbf61ebf93504769e19683d6bd2bfe061f69
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-15 15:28:59 +0000

    login to moj sso if enabled

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,3 +1,4 @@
+const config = require('../../config')
 var moment = require('moment')
 var databaseHelper = require('../helpers/database-setup-for-tests')
 var expect = require('chai').expect
@@ -16,6 +17,17 @@ describe('First time claim viewing flow', () => {
       claimId = ids.claimId
       expenseId1 = ids.expenseId1
       expenseId2 = ids.expenseId2
+    })
+    .then(function () {
+      // IF SSO ENABLED LOGIN TO SSO
+      if (config.AUTHENTICATION_ENABLED === 'true') {
+        return browser.url(config.TOKEN_HOST)
+          .waitForExist('#user_email')
+          .setValue('#user_email', config.TEST_SSO_EMAIL)
+          .setValue('#user_password', config.TEST_SSO_PASSWORD)
+          .click('[name="commit"]')
+          .waitForExist('[href="/users/sign_out"]')
+      }
     })
   })
 
