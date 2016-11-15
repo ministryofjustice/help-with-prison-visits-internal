5c175dcc846299e5b25c2dafa2434fff2d00770d
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-15 16:33:16 +0000

    added caseworker user details to claim

---
+++ migrations/20161115143504_add-claim-column-caseworker.js
@@ -0,0 +1,13 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.table('Claim', function (table) {
+    table.string('AssistedDigitalCaseworker', 100)
+    table.string('Caseworker', 100)
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.table('Claim', function (table) {
+    table.dropColumn('AssistedDigitalCaseworker')
+    table.string('Caseworker', 100)
+  })
+}
