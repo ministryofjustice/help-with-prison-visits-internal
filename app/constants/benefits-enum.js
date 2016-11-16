3ad6de184bf0346a89e3c4ad2f9ae77d9941de65
Michael Hall <m.hall@kainos.com>
2016-11-16 09:16:22 +0000

    Feature/apvs 348/view and approve benefit documents (#57)
    * Added evidence documents to benefits
    
    * Fixed bug in get claim and fixed e2e test
    
    * Updated database test helper and integration test for benefit documents

--- app/constants/benefits-enum.js
+++ app/constants/benefits-enum.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'income-support': {'displayName': 'Income Support'},
-  'jobseekers-allowance': {'displayName': 'Jobseeker’s Allowance'},
-  'employment-support': {'displayName': 'Employment and Support Allowance'},
-  'universal-credit': {'displayName': 'Universal Credit'},
-  'working-tax-credit': {'displayName': 'Working Tax credits with disability or Child tax credit'},
-  'pension-credit': {'displayName': 'Pension Credit'},
-  'hc2': {'displayName': 'Health Certificate (HC2)'},
-  'hc3': {'displayName': 'Health Certificate (HC3)'},
-  'nhs-tax-credit': {'displayName': 'NHS Tax Credit Exemption Certificate'}
+  'income-support': {'documentUploadRequired': false, 'displayName': 'Income Support'},
+  'jobseekers-allowance': {'documentUploadRequired': false, 'displayName': 'Jobseeker’s Allowance (Income based)'},
+  'employment-support': {'documentUploadRequired': false, 'displayName': 'Employment and Support Allowance (Income related)'},
+  'universal-credit': {'documentUploadRequired': false, 'displayName': 'Universal Credit (and you earn less than £1,250 a month)'},
+  'working-tax-credit': {'documentUploadRequired': true, 'displayName': 'Working Tax credits with disability or Child tax credit (and you earn less than £17,474 a year)'},
+  'pension-credit': {'documentUploadRequired': true, 'displayName': 'Pension Credit'},
+  'hc2': {'documentUploadRequired': true, 'displayName': 'Health Certificate (HC2)'},
+  'hc3': {'documentUploadRequired': true, 'displayName': 'Health Certificate (HC3)'},
+  'nhs-tax-credit': {'documentUploadRequired': true, 'displayName': 'NHS Tax Credit Exemption Certificate'}
 }
