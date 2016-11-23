8d44b59432049898f29d2d5bdf7ff85343f76ba3
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-23 14:21:01 +0000

    add claim event on claim decision

---
+++ test/integration/services/data/test-insert-claim-event.js
@@ -0,0 +1,49 @@
+const expect = require('chai').expect
+const config = require('../../../../knexfile').migrations
+const knex = require('knex')(config)
+const moment = require('moment')
+const databaseHelper = require('../../../helpers/database-setup-for-tests')
+
+const insertClaimEvent = require('../../../../app/services/data/insert-claim-event')
+
+const REFERENCE = 'CLAIMEV'
+const EVENT = 'TEST-EVENT'
+const ADDITIONAL_DATA = 'ADDITIONAL_DATA'
+const NOTE = 'NOTE'
+const CASEWORKER = 'CASEWORKER'
+const IS_INTERNAL = false
+
+var eligibilityId
+var claimId
+
+describe('services/data/insert-claim-event', function () {
+  before(function () {
+    return databaseHelper.insertTestData(REFERENCE, moment().toDate(), 'Test').then(function (ids) {
+      claimId = ids.claimId
+      eligibilityId = ids.eligibilityId
+    })
+  })
+
+  it('should insert a new task to send the first time claim notification', function () {
+    return insertClaimEvent(REFERENCE, eligibilityId, claimId, EVENT, ADDITIONAL_DATA, NOTE, CASEWORKER, IS_INTERNAL)
+      .then(function () {
+        return knex.first().from('IntSchema.ClaimEvent')
+          .where({EligibilityId: eligibilityId, Reference: REFERENCE, ClaimId: claimId})
+          .then(function (claimEvent) {
+            expect(claimEvent.EligibilityId).to.equal(eligibilityId)
+            expect(claimEvent.Reference).to.equal(REFERENCE)
+            expect(claimEvent.ClaimId).to.equal(claimId)
+            expect(claimEvent.DateAdded).to.be.within(moment().add(-2, 'minutes').toDate(), moment().add(2, 'minutes').toDate())
+            expect(claimEvent.Event).to.equal(EVENT)
+            expect(claimEvent.AdditionalData).to.equal(ADDITIONAL_DATA)
+            expect(claimEvent.Note).to.equal(NOTE)
+            expect(claimEvent.Caseworker).to.equal(CASEWORKER)
+            expect(claimEvent.IsInternal).to.equal(IS_INTERNAL)
+          })
+      })
+  })
+
+  after(function () {
+    return databaseHelper.deleteAll(REFERENCE)
+  })
+})
