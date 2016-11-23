67df8b7b831da0900186613e5a4c229651cdd972
Thomas Swann <ts.swann@gmail.com>
2016-11-23 21:28:55 +0000

    Added Direct Payment File table

---
+++ migrations/20161123212813_add-direct-payment-file.js
@@ -0,0 +1,18 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('DirectPaymentFile', function (table) {
+    table.increments('PaymentFileId')
+    table.integer('BatchId').notNullable()
+    table.string('FileType', 20).notNullable()
+    table.dateTime('DateCreated').notNullable()
+    table.string('Filepath', 250).notNullable()
+    table.boolean('IsEnabled').defaultTo(true)
+  })
+  .catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('DirectPaymentFile')
+}
