51d98aa6212105d9b7ef9cab542a3aa7423ba714
Michael Hall <m.hall@kainos.com>
2016-11-29 14:27:09 +0000

    UX changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,8 @@ module.exports = function (router) {
     authorisation.isAuthenticated(req)
 
     res.render('index', {
-      title: 'APVS index'
+      title: 'APVS index',
+      active: req.query.status
     })
   })
 
