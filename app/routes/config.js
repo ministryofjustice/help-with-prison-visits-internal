e4dc4af2d8f4c0909b99f79a2fd66e29bc7450fc
Paul Wright <p.wright@kainos.com>
2016-12-06 14:10:33 +0000

    apvs-385 - pull config from database

--- app/routes/config.js
+++ app/routes/config.js
@@ -1,11 +1,30 @@
 const authorisation = require('../services/authorisation')
+const getAutoApprovalConfig = require('../services/data/get-auto-approval-config')
+// const updateAutoApprovalConfig = require('../services/data/update-auto-approval-config')
 
 module.exports = function (router) {
   router.get('/config', function (req, res) {
     authorisation.isAdmin(req)
 
-    res.render('config', {
-      title: 'APVS Configuration'
-    })
+    getAutoApprovalConfig()
+      .then(function (autoApprovalConfig) {
+        res.render('config', {
+          title: 'APVS Configuration',
+          autoApprovalConfig: autoApprovalConfig
+        })
+      })
+  })
+
+  router.post('/config', function (req, res) {
+    authorisation.isAdmin(req)
+
+    console.dir(req.body)
+
+    // TODO: construct domain object and check for validation errors
+
+    // TODO: persist in db
+    // updateAutoApprovalConfig()
+
+    res.redirect('/config')
   })
 }
