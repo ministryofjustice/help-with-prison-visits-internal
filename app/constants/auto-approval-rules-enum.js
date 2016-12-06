bad542662991029da52a0c7b8f3c4b3f5ececf5a
Paul Wright <p.wright@kainos.com>
2016-12-06 13:27:48 +0000

    apvs-385 - add config table and enum

---
+++ app/constants/auto-approval-rules-enum.js
@@ -0,0 +1,12 @@
+module.exports = {
+  AUTO_APPROVAL_ENABLED: {
+    value: 'auto-approval-enabled',
+    displayName: 'Auto Approval Enabled',
+    description: 'Controls auto approval globally'
+  },
+  AUTO_APPROVAL_COST_VARIANCE: {
+    value: 'auto-approval-cost-variance',
+    displayName: 'Auto Approval Cost Variance',
+    description: 'Controls the cost variance used for auto approval'
+  }
+}
