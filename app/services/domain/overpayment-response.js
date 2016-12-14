8197eff3b987653bb3bf6112e7666ecb16a8ae3e
gdouglas1 <garyd@kainos.com>
2016-12-14 09:18:09 +0000

    Feature/apvs 370/overpayment updates (#137)
    * Added code to retrieve any overpaid claims by reference for view-claim
    
    * Added new partial to view-claim to allow user to mark/unmark overpaid claims
    
    Placeholder design until UX is complete
    
    * Added test for overpayment-response domain object
    
    * Show overpayment amount even if claim is not marked as overpaid
    
    * Moved overpayment amount label to left of textbox
    
    * Added length check of result for test-get-overpaid-claims-by-reference
    
    * Added error highlighting for overpayment
    
    * Updated auto-approval-rules-enum with overpayment check
    
    * Added RemainingOverpaymentAmount column
    
    * Show visit date, total and remaining overpayment amount in warning
    
    * Added RemainingOverpaymentBalance column and update field when overpayment status is set
    
    * Moved overpayment section to below decision section
    
    * Added module to calculate and update remaining overpayment value for claims by reference
    
    * Added call in submit-claim-response to calculate remaining overpayment values
    
    * New + updated tests for updating remaining overpayment amount
    
    * Return overpaid claims in order of date of visit
    
    * Test related updates
    
    * Updates to tests
    
    * Added OverpaymentReason to select fields for get-individual-claim-details
    
    * Removed console.log calls, now includes reason when adding overpayment claim event
    
    * Added thead element to overpaid section, moved heading to directly above table
    
    * Updates after comments
    
    * Removed duplicate function (added in previous merge)
    
    * Fixed note when updating remaining overpayment amount
    
    * Fixed display issue for overpayment fields
    
    * Now inserts claim event when a deduction is applied on a related claim
    
    * Updates from comments

--- app/services/domain/overpayment-response.js
+++ app/services/domain/overpayment-response.js
@@ -1,23 +1,31 @@
 const ValidationError = require('../errors/validation-error')
 const FieldValidator = require('../validators/field-validator')
 const ErrorHandler = require('../validators/error-handler')
+const overpaymentActionEnum = require('../../constants/overpayment-action-enum')
 
 class OverpaymentResponse {
-  constructor (isOverpaid, amount, reason) {
-    this.isOverpaid = isOverpaid
+  constructor (amount, remaining, reason, currentOverpaymentStatus) {
+    this.action = getOverpaymentStatus(amount, remaining, currentOverpaymentStatus)
     this.amount = amount
+    this.remaining = remaining
     this.reason = reason
+
     this.IsValid()
   }
 
   IsValid () {
     var errors = ErrorHandler()
 
-    if (this.isOverpaid) {
+    if (this.action === overpaymentActionEnum.UPDATE || this.action === overpaymentActionEnum.RESOLVE) {
+      FieldValidator(this.remaining, 'overpayment-remaining', errors)
+        .isRequired()
+        .isGreaterThanOrEqualToZero()
+        .isCurrency()
+    } else if (this.action === overpaymentActionEnum.OVERPAID) {
       FieldValidator(this.amount, 'overpayment-amount', errors)
-      .isRequired()
-      .isGreaterThanZero()
-      .isCurrency()
+        .isRequired()
+        .isGreaterThanZero()
+        .isCurrency()
     }
 
     var validationErrors = errors.get()
@@ -26,6 +34,22 @@ class OverpaymentResponse {
       throw new ValidationError(validationErrors)
     }
   }
+}
+
+function getOverpaymentStatus (amount, remaining, claimCurrentlyOverpaid) {
+  var result
+
+  if (claimCurrentlyOverpaid) {
+    if (remaining === '0') {
+      result = overpaymentActionEnum.RESOLVE
+    } else {
+      result = overpaymentActionEnum.UPDATE
+    }
+  } else {
+    result = overpaymentActionEnum.OVERPAID
+  }
+
+  return result
 }
 
 module.exports = OverpaymentResponse
