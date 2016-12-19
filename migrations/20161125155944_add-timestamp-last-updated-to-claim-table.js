bbba305850b3934c6aceb080d7791f6dd3b613b6
Thomas Rowe <thomasrowe101@gmail.com>
2016-12-19 11:19:19 +0000

    Added partial implementation for fixing another rollback issue.

--- migrations/20161125155944_add-timestamp-last-updated-to-claim-table.js
+++ migrations/20161125155944_add-timestamp-last-updated-to-claim-table.js
@@ -2,10 +2,37 @@ exports.up = function (knex, Promise) {
   return knex.schema.table('Claim', function (table) {
     table.timestamp('LastUpdated').defaultTo(knex.fn.now())
   })
+  .catch(function (error) {
+    console.log(error)
+    throw error
+  })
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.table('Claim', function (table) {
-    table.dropColumn('LastUpdated')
-  })
+  return knex.schema
+    // Retrieve the name of the constraint on the LastUpdated column and remove it before dropping the column.
+    .raw(
+      `
+        DECLARE @Constraint varchar(100);
+        SET @Constraint = (
+          SELECT o.name 
+          FROM sysobjects o 
+          INNER JOIN syscolumns c 
+            ON o.id = c.cdefault 
+          INNER JOIN sysobjects t 
+            ON c.id = t.id 
+          WHERE o.xtype = 'D' 
+          AND c.name = 'LastUpdated' 
+        )
+        
+
+      `
+    )
+    .table('Claim', function (table) {
+      table.dropColumn('LastUpdated')
+    })
+    .catch(function (error) {
+      console.log(error)
+      throw error
+    })
 }
