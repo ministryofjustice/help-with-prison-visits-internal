fa3f5fbba7c6ccb3fb59b9e88aae8e65d1c36bde
Thomas Rowe <thomasrowe101@gmail.com>
2016-12-19 12:10:25 +0000

    Fixed rollback issue.

--- migrations/20161125155944_add-timestamp-last-updated-to-claim-table.js
+++ migrations/20161125155944_add-timestamp-last-updated-to-claim-table.js
@@ -13,19 +13,25 @@ exports.down = function (knex, Promise) {
     // Retrieve the name of the constraint on the LastUpdated column and remove it before dropping the column.
     .raw(
       `
-        DECLARE @Constraint varchar(100);
-        SET @Constraint = (
-          SELECT o.name 
-          FROM sysobjects o 
-          INNER JOIN syscolumns c 
-            ON o.id = c.cdefault 
-          INNER JOIN sysobjects t 
-            ON c.id = t.id 
-          WHERE o.xtype = 'D' 
-          AND c.name = 'LastUpdated' 
-        )
+        DECLARE @table_id AS INT
+        DECLARE @name_column_id AS INT
+        DECLARE @sql nvarchar(255) 
         
-
+        -- Find table id
+        SET @table_id = OBJECT_ID('IntSchema.Claim')
+        
+        -- Find name column id
+        SELECT @name_column_id = column_id
+        FROM sys.columns
+        WHERE object_id = @table_id
+        AND name = 'LastUpdated'
+        
+        -- Remove default constraint from column
+        SELECT @sql = 'ALTER TABLE IntSchema.Claim DROP CONSTRAINT ' + D.name
+        FROM sys.default_constraints AS D
+        WHERE D.parent_object_id = @table_id
+        AND D.parent_column_id = @name_column_id
+        EXECUTE sp_executesql @sql
       `
     )
     .table('Claim', function (table) {
