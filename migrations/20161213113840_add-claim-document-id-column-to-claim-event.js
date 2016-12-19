5c2a2d9dcba078a1c77e9c7046478229f4d4cba2
Thomas Rowe <thomasrowe101@gmail.com>
2016-12-19 12:14:24 +0000

    Fixed formatting error.

--- migrations/20161213113840_add-claim-document-id-column-to-claim-event.js
+++ migrations/20161213113840_add-claim-document-id-column-to-claim-event.js
@@ -10,7 +10,7 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema.table('ClaimEvent', function (table) {
-    table.dropForeign('ClaimDocumentId');
+    table.dropForeign('ClaimDocumentId')
     table.dropColumn('ClaimDocumentId')
   })
   .catch(function (error) {
