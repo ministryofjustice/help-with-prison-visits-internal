92ffd1b8ec9e8db04cda3cfce3a4847a6f5274d8
Paul Wright <p.wright@kainos.com>
2017-01-05 11:19:52 +0000

    update tests

--- test/integration/services/data/test-get-claim-list-for-search.js
+++ test/integration/services/data/test-get-claim-list-for-search.js
@@ -7,7 +7,6 @@ var knex = require('knex')(config)
 var testData
 var reference = 'SEARCH1'
 var date
-var claimId
 var getClaimsListForSearch = require('../../../../app/services/data/get-claim-list-for-search')
 
 describe('services/data/get-claim-list-for-search', function () {
@@ -15,9 +14,6 @@ describe('services/data/get-claim-list-for-search', function () {
     date = dateFormatter.now()
     testData = databaseHelper.getTestData(reference, 'TESTING')
     return databaseHelper.insertTestData(reference, date.toDate(), 'TESTING')
-      .then(function (ids) {
-        claimId = ids.claimId
-      })
   })
 
   it('should return inserted claim when a reference number is provided', function () {
@@ -59,51 +55,38 @@ describe('services/data/get-claim-list-for-search', function () {
   })
 
   describe('name', function () {
-    var testFirstName = 'Search'
-    var testLastName = 'Test'
+    var testFirstName = 'John'
+    var testLastName = 'Smith'
     var name = testFirstName + ' ' + testLastName
-
-    before(function () {
-      // Change name so other test rows are not returned
-      return knex('IntSchema.Visitor')
-        .update({'FirstName': testFirstName, 'LastName': testLastName})
-        .where('Reference', reference)
-    })
 
     it('should return correct number of claims when a full name is provided', function () {
       return getClaimsListForSearch(name, 0, 10)
         .then(function (result) {
-          expect(result.claims.length, 'length should equal 1').to.equal(1)
-          expect(result.claims[0].Reference).to.equal(reference)
-          expect(result.claims[0].ClaimId).to.equal(claimId)
-          expect(result.total.Count, 'count should equal 1').to.equal(1)
+          var claimsWithCurrentReference = result.claims.filter(function (claim) {
+            return claim.Reference === reference
+          })
+          expect(claimsWithCurrentReference.length).to.equal(1)
         })
     })
 
     it('should return correct number of claims when a first name is provided', function () {
       return getClaimsListForSearch(testFirstName, 0, 10)
         .then(function (result) {
-          expect(result.claims.length, 'length should equal 1').to.equal(1)
-          expect(result.claims[0].Reference).to.equal(reference)
-          expect(result.claims[0].ClaimId).to.equal(claimId)
-          expect(result.total.Count, 'count should equal 1').to.equal(1)
+          var claimsWithCurrentReference = result.claims.filter(function (claim) {
+            return claim.Reference === reference
+          })
+          expect(claimsWithCurrentReference.length).to.equal(1)
         })
     })
 
     it('should return correct number of claims when a last name is provided', function () {
       return getClaimsListForSearch(testLastName, 0, 10)
         .then(function (result) {
-          expect(result.claims.length, 'length should equal 1').to.equal(1)
-          expect(result.claims[0].Reference).to.equal(reference)
-          expect(result.claims[0].ClaimId).to.equal(claimId)
-          expect(result.total.Count, 'count should equal 1').to.equal(1)
+          var claimsWithCurrentReference = result.claims.filter(function (claim) {
+            return claim.Reference === reference
+          })
+          expect(claimsWithCurrentReference.length).to.equal(1)
         })
-    })
-
-    after(function () {
-      return knex('IntSchema.Visitor')
-        .update({'FirstName': 'John', 'LastName': 'Smith'})
-        .where('Reference', reference)
     })
   })
 
