c126b91da53d99d3b0ffa1ce4ed5038ef8c79fda
Gary Douglas <garyd@kainos.com>
2017-01-06 15:35:03 +0000

    Updated tests so that they will work when data already exists in db

--- test/integration/services/data/dashboard/test-get-auto-approved-claim-count.js
+++ test/integration/services/data/dashboard/test-get-auto-approved-claim-count.js
@@ -25,54 +25,68 @@ var twoMonthsAgo = moment().subtract(2, 'months').toDate()
 var threeMonthsAgo = moment().subtract(3, 'months').toDate()
 var fourMonthsAgo = moment().subtract(4, 'months').toDate()
 
+var todayCount
+var yesterdayCount
+var last7DaysCount
+var oneMonthAgoCount
+var twoMonthsAgoCount
+var threeMonthsAgoCount
+var fourMonthsAgoCount
+
 describe('services/data/dashboard/get-auto-approved-claim-count', function () {
   describe('module', function () {
     before(function () {
-      return databaseHelper.insertTestData(reference, date, claimStatusEnum.AUTOAPPROVED.value)
-        .then(function (ids) {
-          var eligibilityId = ids.eligibilityId
-          claimId2 = ids.claimId + 1
-          claimId3 = ids.claimId + 2
-          claimId4 = ids.claimId + 3
-          claimId5 = ids.claimId + 4
-          claimId6 = ids.claimId + 5
-          claimId7 = ids.claimId + 6
-          claimId8 = ids.claimId + 7
-          claimId9 = ids.claimId + 8
-          claimId10 = ids.claimId + 9
+      return getCountsBeforeTest()
+        .then(function () {
+          return databaseHelper.insertTestData(reference, date, claimStatusEnum.AUTOAPPROVED.value)
+            .then(function (ids) {
+              var eligibilityId = ids.eligibilityId
+              claimId2 = ids.claimId + 1
+              claimId3 = ids.claimId + 2
+              claimId4 = ids.claimId + 3
+              claimId5 = ids.claimId + 4
+              claimId6 = ids.claimId + 5
+              claimId7 = ids.claimId + 6
+              claimId8 = ids.claimId + 7
+              claimId9 = ids.claimId + 8
+              claimId10 = ids.claimId + 9
 
-          var promises = []
+              var promises = []
 
-          promises.push(databaseHelper.insertClaim(claimId2, eligibilityId, reference, yesterday, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId3, eligibilityId, reference, lastWeek, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId4, eligibilityId, reference, oneMonthAgo, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId5, eligibilityId, reference, twoMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId6, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId7, eligibilityId, reference, fourMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId8, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId9, eligibilityId, reference, oneMonthAgo, claimStatusEnum.REJECTED.value, false))
-          promises.push(databaseHelper.insertClaim(claimId10, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.PENDING.value, false))
+              promises.push(databaseHelper.insertClaim(claimId2, eligibilityId, reference, yesterday, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId3, eligibilityId, reference, lastWeek, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId4, eligibilityId, reference, oneMonthAgo, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId5, eligibilityId, reference, twoMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId6, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId7, eligibilityId, reference, fourMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId8, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId9, eligibilityId, reference, oneMonthAgo, claimStatusEnum.REJECTED.value, false))
+              promises.push(databaseHelper.insertClaim(claimId10, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.PENDING.value, false))
 
-          return Promise.all(promises)
+              return Promise.all(promises)
+            })
         })
     })
 
     it('should return the correct number of Auto-Approved claims submitted today', function () {
-      return checkCount(dashboardFilterEnum.TODAY, 1)
+      var expectedResult = todayCount + 1
+      return checkCount(dashboardFilterEnum.TODAY, expectedResult)
     })
 
     it('should return the correct number of Auto-Approved claims submitted yesterday', function () {
-      return checkCount(dashboardFilterEnum.YESTERDAY, 1)
+      var expectedResult = yesterdayCount + 1
+      return checkCount(dashboardFilterEnum.YESTERDAY, expectedResult)
     })
 
     it('should return the correct number of Auto-Approved claims submitted in the last 7 days', function () {
-      return checkCount(dashboardFilterEnum.LAST_7_DAYS, 3)
+      var expectedResult = last7DaysCount + 1
+      return checkCount(dashboardFilterEnum.LAST_7_DAYS, expectedResult)
     })
 
     it('should return the correct number of Auto-Approved claims submitted in the previous calendar month', function () {
       // Need to amend expected count if test is run before the 7th of the month,
       // because yesterday and last week could potentially be in the previous calendar month
-      var expectedResult = 1
+      var expectedResult = 1 + oneMonthAgoCount
 
       if (moment(yesterday).isSame(oneMonthAgo, 'month')) {
         expectedResult++
@@ -86,15 +100,18 @@ describe('services/data/dashboard/get-auto-approved-claim-count', function () {
     })
 
     it('should return the correct number of Auto-Approved claims submitted in the calendar month before the previous month', function () {
-      return checkCount(dashboardFilterEnum.TWO_MONTHS_AGO, 1)
+      var expectedResult = todayCount + 1
+      return checkCount(dashboardFilterEnum.TWO_MONTHS_AGO, expectedResult)
     })
 
     it('should return the correct number of Auto-Approved claims submitted in the calendar month 3 months ago', function () {
-      return checkCount(dashboardFilterEnum.THREE_MONTHS_AGO, 1)
+      var expectedResult = threeMonthsAgoCount + 1
+      return checkCount(dashboardFilterEnum.THREE_MONTHS_AGO, expectedResult)
     })
 
     it('should return the correct number of Auto-Approved claims submitted in the calendar month 4 months ago', function () {
-      return checkCount(dashboardFilterEnum.FOUR_MONTHS_AGO, 1)
+      var expectedResult = fourMonthsAgoCount + 1
+      return checkCount(dashboardFilterEnum.FOUR_MONTHS_AGO, expectedResult)
     })
 
     after(function () {
@@ -111,4 +128,18 @@ function checkCount (filter, expectedCount) {
     .catch(function (error) {
       throw error
     })
+}
+
+function getCountsBeforeTest () {
+  var promises = []
+
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.TODAY).then(function (result) { todayCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.YESTERDAY).then(function (result) { yesterdayCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.LAST_7_DAYS).then(function (result) { last7DaysCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.ONE_MONTH_AGO).then(function (result) { oneMonthAgoCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.TWO_MONTHS_AGO).then(function (result) { twoMonthsAgoCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.THREE_MONTHS_AGO).then(function (result) { threeMonthsAgoCount = result[0].Count }))
+  promises.push(getAutoApprovedClaimCount(dashboardFilterEnum.FOUR_MONTHS_AGO).then(function (result) { fourMonthsAgoCount = result[0].Count }))
+
+  return Promise.all(promises)
 }
--- test/integration/services/data/dashboard/test-get-paid-claim-count.js
+++ test/integration/services/data/dashboard/test-get-paid-claim-count.js
@@ -18,6 +18,7 @@ var claimId8
 var claimId9
 var claimId10
 
+
 var date = moment().toDate()
 var yesterday = moment().subtract(1, 'days').toDate()
 var lastWeek = moment().subtract(7, 'days').toDate()
@@ -26,54 +27,68 @@ var twoMonthsAgo = moment().subtract(2, 'months').toDate()
 var threeMonthsAgo = moment().subtract(3, 'months').toDate()
 var fourMonthsAgo = moment().subtract(4, 'months').toDate()
 
+var todayCount
+var yesterdayCount
+var last7DaysCount
+var oneMonthAgoCount
+var twoMonthsAgoCount
+var threeMonthsAgoCount
+var fourMonthsAgoCount
+
 describe('services/data/dashboard/get-paid-claim-count', function () {
   describe('module', function () {
     before(function () {
-      return databaseHelper.insertTestData(reference, date, claimStatusEnum.AUTOAPPROVED.value)
-        .then(function (ids) {
-          var eligibilityId = ids.eligibilityId
-          claimId2 = ids.claimId + 1
-          claimId3 = ids.claimId + 2
-          claimId4 = ids.claimId + 3
-          claimId5 = ids.claimId + 4
-          claimId6 = ids.claimId + 5
-          claimId7 = ids.claimId + 6
-          claimId8 = ids.claimId + 7
-          claimId9 = ids.claimId + 8
-          claimId10 = ids.claimId + 9
+      return getCountsBeforeTest()
+        .then(function () {
+          return databaseHelper.insertTestData(reference, date, claimStatusEnum.AUTOAPPROVED.value)
+            .then(function (ids) {
+              var eligibilityId = ids.eligibilityId
+              claimId2 = ids.claimId + 1
+              claimId3 = ids.claimId + 2
+              claimId4 = ids.claimId + 3
+              claimId5 = ids.claimId + 4
+              claimId6 = ids.claimId + 5
+              claimId7 = ids.claimId + 6
+              claimId8 = ids.claimId + 7
+              claimId9 = ids.claimId + 8
+              claimId10 = ids.claimId + 9
 
-          var promises = []
+              var promises = []
 
-          promises.push(databaseHelper.insertClaim(claimId2, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, true, null))
-          promises.push(databaseHelper.insertClaim(claimId3, eligibilityId, reference, lastWeek, claimStatusEnum.AUTOAPPROVED.value, false, null, null, false, paymentStatusProcessed))
-          promises.push(databaseHelper.insertClaim(claimId4, eligibilityId, reference, oneMonthAgo, claimStatusEnum.APPROVED.value, false, null, null, false, paymentStatusProcessed))
-          promises.push(databaseHelper.insertClaim(claimId5, eligibilityId, reference, twoMonthsAgo, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, true, null))
-          promises.push(databaseHelper.insertClaim(claimId6, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false, null, null, false, paymentStatusProcessed))
-          promises.push(databaseHelper.insertClaim(claimId7, eligibilityId, reference, fourMonthsAgo, claimStatusEnum.APPROVED.value, false, null, null, false, paymentStatusProcessed))
-          promises.push(databaseHelper.insertClaim(claimId8, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, false, null))
-          promises.push(databaseHelper.insertClaim(claimId9, eligibilityId, reference, oneMonthAgo, claimStatusEnum.REJECTED.value, false, null, null, false, null))
-          promises.push(databaseHelper.insertClaim(claimId10, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.PENDING.value, false, null, null, false, null))
+              promises.push(databaseHelper.insertClaim(claimId2, eligibilityId, reference, date, claimStatusEnum.APPROVED.value, false, null, null, true, paymentStatusProcessed))
+              promises.push(databaseHelper.insertClaim(claimId3, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, true, null))
+              promises.push(databaseHelper.insertClaim(claimId4, eligibilityId, reference, lastWeek, claimStatusEnum.AUTOAPPROVED.value, false, null, null, false, paymentStatusProcessed))
+              promises.push(databaseHelper.insertClaim(claimId5, eligibilityId, reference, oneMonthAgo, claimStatusEnum.APPROVED.value, false, null, null, false, paymentStatusProcessed))
+              promises.push(databaseHelper.insertClaim(claimId6, eligibilityId, reference, twoMonthsAgo, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, true, null))
+              promises.push(databaseHelper.insertClaim(claimId7, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.AUTOAPPROVED.value, false, null, null, false, paymentStatusProcessed))
+              promises.push(databaseHelper.insertClaim(claimId8, eligibilityId, reference, fourMonthsAgo, claimStatusEnum.APPROVED.value, false, null, null, false, paymentStatusProcessed))
+              promises.push(databaseHelper.insertClaim(claimId9, eligibilityId, reference, yesterday, claimStatusEnum.APPROVED_ADVANCE_CLOSED.value, false, null, null, false, null))
+              promises.push(databaseHelper.insertClaim(claimId10, eligibilityId, reference, threeMonthsAgo, claimStatusEnum.PENDING.value, false, null, null, false, null))
 
-          return Promise.all(promises)
+              return Promise.all(promises)
+            })
         })
     })
 
     it('should return the correct number of Paid claims submitted today', function () {
-      return checkCount(dashboardFilterEnum.TODAY, 0)
+      var expectedResult = todayCount + 1
+      return checkCount(dashboardFilterEnum.TODAY, expectedResult)
     })
 
     it('should return the correct number of Paid claims submitted yesterday', function () {
-      return checkCount(dashboardFilterEnum.YESTERDAY, 1)
+      var expectedResult = yesterdayCount + 1
+      return checkCount(dashboardFilterEnum.YESTERDAY, expectedResult)
     })
 
     it('should return the correct number of Paid claims submitted in the last 7 days', function () {
-      return checkCount(dashboardFilterEnum.LAST_7_DAYS, 2)
+      var expectedResult = last7DaysCount + 3
+      return checkCount(dashboardFilterEnum.LAST_7_DAYS, expectedResult)
     })
 
     it('should return the correct number of Paid claims submitted in the previous calendar month', function () {
       // Need to amend expected count if test is run before the 7th of the month,
       // because yesterday and last week could potentially be in the previous calendar month
-      var expectedResult = 1
+      var expectedResult = oneMonthAgoCount + 1
 
       if (moment(yesterday).isSame(oneMonthAgo, 'month')) {
         expectedResult++
@@ -87,15 +102,18 @@ describe('services/data/dashboard/get-paid-claim-count', function () {
     })
 
     it('should return the correct number of Paid claims submitted in the calendar month before the previous month', function () {
-      return checkCount(dashboardFilterEnum.TWO_MONTHS_AGO, 1)
+      var expectedResult = twoMonthsAgoCount + 1
+      return checkCount(dashboardFilterEnum.TWO_MONTHS_AGO, expectedResult)
     })
 
     it('should return the correct number of Paid claims submitted in the calendar month 3 months ago', function () {
-      return checkCount(dashboardFilterEnum.THREE_MONTHS_AGO, 1)
+      var expectedResult = threeMonthsAgoCount + 1
+      return checkCount(dashboardFilterEnum.THREE_MONTHS_AGO, expectedResult)
     })
 
     it('should return the correct number of Paid claims submitted in the calendar month 4 months ago', function () {
-      return checkCount(dashboardFilterEnum.FOUR_MONTHS_AGO, 1)
+      var expectedResult = fourMonthsAgoCount + 1
+      return checkCount(dashboardFilterEnum.FOUR_MONTHS_AGO, expectedResult)
     })
 
     after(function () {
@@ -112,5 +130,18 @@ function checkCount (filter, expectedCount) {
     .catch(function (error) {
       throw error
     })
-  // console.log(getPaidClaimCount(filter))
+}
+
+function getCountsBeforeTest () {
+  var promises = []
+
+  promises.push(getPaidClaimCount(dashboardFilterEnum.TODAY).then(function (result) { todayCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.YESTERDAY).then(function (result) { yesterdayCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.LAST_7_DAYS).then(function (result) { last7DaysCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.ONE_MONTH_AGO).then(function (result) { oneMonthAgoCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.TWO_MONTHS_AGO).then(function (result) { twoMonthsAgoCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.THREE_MONTHS_AGO).then(function (result) { threeMonthsAgoCount = result[0].Count }))
+  promises.push(getPaidClaimCount(dashboardFilterEnum.FOUR_MONTHS_AGO).then(function (result) { fourMonthsAgoCount = result[0].Count }))
+
+  return Promise.all(promises)
 }
