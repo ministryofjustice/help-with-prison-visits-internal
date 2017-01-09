0a4c328221110590d7498570ab09042a7b295786
Thomas Swann <ts.swann@gmail.com>
2017-01-09 12:59:47 +0000

    Adding assertions for sub-second query response time

--- test/performance/claim-list-load-tests.js
+++ test/performance/claim-list-load-tests.js
@@ -4,9 +4,11 @@ var getClaimsListAndCount = require('../../app/services/data/get-claim-list-and-
 
 const DEFAULT_CLAIM_LIST_SIZE = 500
 const DEFAULT_APPROVED_CLAIM_SIZE = 10000
+const LIST_QUERY_MAX_TIME_MS = 1000
 
 describe('Test performance of claim list queries under a known record load', () => {
   before(function () {
+    console.log('Deleting any existing data in IntSchema')
     return perfTestHelper.deleteAll()
       .then(function () {
         console.log('Inserting ' + DEFAULT_CLAIM_LIST_SIZE + ' NEW claim records')
@@ -39,44 +41,68 @@ describe('Test performance of claim list queries under a known record load', () 
   })
 
   it('should return NEW records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('NEW', false, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
   it('should return UPDATED records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('UPDATED', false, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
   it('should return PENDING records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('PENDING', false, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
   it('should return ADVANCE records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('NEW', true, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
   it('should return ADVANCE APPROVED records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('APPROVED', true, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
   it('should return ADVANCE UPDATED records list and count', function () {
+    var start = process.hrtime()
     return getClaimsListAndCount('UPDATED', true, 0, 10).then(function (result) {
+      var diff = process.hrtime(start)
       expect(result.total.Count).to.equal(DEFAULT_CLAIM_LIST_SIZE)
       expect(result.claims.length).to.equal(10)
+      console.log('Execution time: %ds %dms', diff[0], diff[1] / 1000000)
+      expect(diff[1] / 1000000).to.be.lessThan(LIST_QUERY_MAX_TIME_MS)
     })
   })
 
