08d96eaba99a0d0d2a9c60886ade6a56577337b6
Gary Douglas <garyd@kainos.com>
2017-01-10 14:10:43 +0000

    Tidy-up

--- app/services/export-search-results.js
+++ app/services/export-search-results.js
@@ -12,17 +12,17 @@ const NAME_HEADER = 'Name'
 const PRISON_NAME_HEADER = 'Prison Name'
 const PRISONER_RELATIONSHIP_HEADER = 'Prisoner Relationship'
 const CHILD_COUNT_HEADER = 'Child Count'
-const HAS_MEDICAL_ESCORT_HEADER = 'Has Medical Escort?'
+const HAS_ESCORT_HEADER = 'Has Escort?'
 const PRISON_REGION_HEADER = 'Region'
 const VISIT_DATE_HEADER = 'Visit Date'
 const CLAIM_SUBMISSION_DATE_HEADER = 'Claim Submission Date'
-const CLAIMED_HEADER = 'Has Claimed'
+const BENEFIT_CLAIMED_HEADER = 'Benefit Claimed'
 const ASSISTED_DIGITAL_CASEWORKER_HEADER = 'Assisted Digital Caseworker'
 const CASEWORKER_HEADER = 'Caseworker'
-const IS_TRUSTED_HEADER = 'Trusted'
+const IS_TRUSTED_HEADER = 'Trusted?'
 const CLAIM_STATUS_HEADER = 'Status'
 const DATE_REVIEWED_BY_CASEWORKER_HEADER = 'Date Reviewed by Caseworker'
-const IS_ADVANCE_CLAIM_HEADER = 'Is Advance Claim'
+const IS_ADVANCE_CLAIM_HEADER = 'Is Advance Claim?'
 const TOTAL_AMOUNT_PAID_HEADER = 'Total amount paid'
 
 module.exports = function (searchCriteria) {
@@ -43,31 +43,31 @@ function transformData (data) {
     var promises = []
 
     promises.push(getClaimChildCount(claim.ClaimId))
-    // promises.push(getClaimEscort(claim.ClaimId))
+    promises.push(getClaimEscort(claim.ClaimId))
 
     return Promise.all(promises)
       .then(function (result) {
         var returnValue = {}
 
         var childCount = result[0][0].Count
-        var claimEscort = result[1][0]
+        var claimEscortCount = result[1][0].Count
 
         returnValue[NAME_HEADER] = claim.Name
-        // returnValue[PRISON_NAME_HEADER] = displayHelper.getPrisonDisplayName(claim.NameOfPrison)
-        // returnValue[PRISONER_RELATIONSHIP_HEADER] = prisonerRelationshipEnum[claim.Relationship].displayName
+        returnValue[PRISON_NAME_HEADER] = displayHelper.getPrisonDisplayName(claim.NameOfPrison)
+        returnValue[PRISONER_RELATIONSHIP_HEADER] = prisonerRelationshipEnum[claim.Relationship].displayName
         returnValue[CHILD_COUNT_HEADER] = childCount
-        // returnValue[HAS_MEDICAL_ESCORT_HEADER] = getClaimEscort(claim.ClaimId).length > 0 ? 'Y' : 'N'
+        returnValue[HAS_ESCORT_HEADER] = claimEscortCount > 0 ? 'Y' : 'N'
         returnValue[VISIT_DATE_HEADER] = dateHelper.shortDate(claim.DateOfJourney)
         returnValue[CLAIM_SUBMISSION_DATE_HEADER] = dateHelper.shortDate(claim.DateSubmitted)
-        // returnValue[PRISON_REGION_HEADER] = displayHelper.getPrisonRegion(claim.NameOfPrison)
-        returnValue[CLAIMED_HEADER] = claim.PaymentStatus === 'PROCESSED' ? 'Y' : 'N'
+        returnValue[PRISON_REGION_HEADER] = displayHelper.getPrisonRegion(claim.NameOfPrison)
+        returnValue[BENEFIT_CLAIMED_HEADER] = displayHelper.getBenefitDisplayName(claim.Benefit)
         returnValue[ASSISTED_DIGITAL_CASEWORKER_HEADER] = claim.AssistedDigitalCaseworker
         returnValue[CASEWORKER_HEADER] = claim.Caseworker
         returnValue[IS_TRUSTED_HEADER] = claim.IsTrusted ? 'Y' : 'N'
         returnValue[CLAIM_STATUS_HEADER] = claim.Status
-        // returnValue[DATE_REVIEWED_BY_CASEWORKER_HEADER] = dateHelper.shortDate(claim.DateReviewed)
+        returnValue[DATE_REVIEWED_BY_CASEWORKER_HEADER] = claim.DateReviewed ? dateHelper.shortDate(claim.DateReviewed) : null
         returnValue[IS_ADVANCE_CLAIM_HEADER] = claim.IsAdvanceClaim ? 'Y' : 'N'
-        returnValue[TOTAL_AMOUNT_PAID_HEADER] = claim.BankPaymentAmount
+        returnValue[TOTAL_AMOUNT_PAID_HEADER] = claim.BankPaymentAmount || 0
 
         return returnValue
       })
