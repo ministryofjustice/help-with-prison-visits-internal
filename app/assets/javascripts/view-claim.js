3078ba57b42b9c631e79379950f57ff275705bde
Haresh Vekriya <Haresh.1994@hotmail.com>
2017-01-11 12:07:04 +0000

    Chore/Cannot preview sum of accepted amount before making decision (#172)
    * added the front-end script to preview sum before making claim decsion
    
    * removed comment
    
    * Fixed bugs after merge review and added fixed decimal functionaility

--- app/assets/javascripts/view-claim.js
+++ app/assets/javascripts/view-claim.js
@@ -3,10 +3,44 @@ $('.claim-expense-status').change(function () {
   var value = $(this).val()
   if (value === 'APPROVED-DIFF-AMOUNT' || value === 'MANUALLY-PROCESSED') {
     show(`#claim-expense-${id}-approvedcost`)
+    $(this).next('input').on('input').addClass('approved-amount')
+    $(this).parent().parent().find('td.cost').removeClass('approved-amount')
+    $('input.approved-amount').on('input', function () {
+      totalApproved()
+    })
+  } else if (value === 'APPROVED') {
+    hide(`#claim-expense-${id}-approvedcost`)
+    $(this).parent().parent().find('td.cost').addClass('approved-amount')
+    $(this).next('input').on('input').removeClass('approved-amount')
   } else {
     hide(`#claim-expense-${id}-approvedcost`)
-  }
+    $(this).parent().parent().find('td.cost').removeClass('approved-amount')
+    $(this).next('input').on('input').removeClass('approved-amount')
+  } totalApproved()
 })
+
+$(function () {
+  $('.claim-expense-status').next('input').addClass('approved-amount')
+  $('input.approved-amount').on('input', function () {
+    totalApproved()
+  })
+})
+
+function totalApproved () {
+  var approvedCost = 0
+  var manuallyProcessed = 0
+  $('input.approved-amount').each(function () {
+    if (!isNaN(this.value) && this.value.length !== 0) {
+      manuallyProcessed += parseFloat(this.value)
+    }
+  })
+
+  $('td.approved-amount').each(function () {
+    approvedCost += +$(this).text().replace('£', '')
+  })
+
+  $('.claim-expense-approvedCostText').text('£' + (approvedCost + manuallyProcessed).toFixed(2))
+}
 
 function hide (element) {
   $(element).hide()
