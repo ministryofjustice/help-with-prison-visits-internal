276fff07433952a99bf5fe862ffa3efccf1dfea2
stevenalexander <steven.william.alexander@googlemail.com>
2017-01-20 16:37:58 +0000

    add sso to smoke test (#229)

--- test/e2e-smoke/dashboard-smoke.js
+++ test/e2e-smoke/dashboard-smoke.js
@@ -1,4 +1,18 @@
+const config = require('../../config')
+
 describe('Smoke test', () => {
+  before(function () {
+    // IF SSO ENABLED LOGIN TO SSO
+    if (config.AUTHENTICATION_ENABLED === 'true') {
+      return browser.url(config.TOKEN_HOST)
+        .waitForExist('#user_email')
+        .setValue('#user_email', config.TEST_SSO_EMAIL)
+        .setValue('#user_password', config.TEST_SSO_PASSWORD)
+        .click('[name="commit"]')
+        .waitForExist('[href="/users/sign_out"]')
+    }
+  })
+
   it('should display the dashboard which calls the database', () => {
     return browser.url('/dashboard')
 
