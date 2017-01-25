d27a2b8b163e0ad23dcd854c2619290c3b1a9d80
Gary Douglas <garyd@kainos.com>
2017-01-25 14:41:13 +0000

    Minor fix

--- app/routes/claim/view-claim.js
+++ app/routes/claim/view-claim.js
@@ -201,6 +201,7 @@ function renderViewClaimPage (claimId, res) {
         displayHelper: displayHelper,
         duplicates: data.duplicates,
         claimEvents: data.claimEvents,
+        deductions: data.deductions,
         overpaidClaims: data.overpaidClaims,
         claimDecisionEnum: claimDecisionEnum
       })
@@ -231,7 +232,7 @@ function handleError (error, req, res, updateConflict, next) {
           displayHelper: displayHelper,
           claimDecision: req.body,
           receiptRequiredEnum: receiptRequiredEnum,
-          deductions: displayHelper.processDeductionAmounts(data.deductions),
+          deductions: data.deductions,
           duplicates: data.duplicates,
           claimEvents: data.claimEvents,
           overpaidClaims: data.overpaidClaims,
