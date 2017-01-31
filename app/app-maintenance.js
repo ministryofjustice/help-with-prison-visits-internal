5779a08dcb534f60bd9e69cc36f5e0a3312a4c07
Steven Alexander <steven.william.alexander@googlemail.com>
2017-01-31 12:10:20 +0000

    chore - added maintenance mode with cut down app middleware

---
+++ app/app-maintenance.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const nunjucks = require('express-nunjucks')
+const path = require('path')
+const favicon = require('serve-favicon')
+
+var app = express()
+var serviceName = 'Assisted Prison Visit Service'
+
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'views'))
+
+nunjucks(app, {
+  watch: false,
+  noCache: false
+})
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_template')))
+app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_frontend_toolkit')))
+app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'images', 'favicon.ico')))
+
+// Send assetPath to all views.
+app.use(function (req, res, next) {
+  res.locals.asset_path = '/public/'
+  next()
+})
+
+// Add variables that are available in all views.
+app.use(function (req, res, next) {
+  res.locals.serviceName = serviceName
+  next()
+})
+
+// Display maintenance page
+app.use(function (req, res, next) {
+  res.render('includes/maintenance')
+})
+
+module.exports = app
