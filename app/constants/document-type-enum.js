cf749f97f4c0f81fed751cdc3c62ef8cb52b5d5c
Michael Hall <m.hall@kainos.com>
2017-02-24 10:44:33 +0000

    Added universal credit to doc type & updated benefit enum (#293)

--- app/constants/document-type-enum.js
+++ app/constants/document-type-enum.js
@@ -5,5 +5,6 @@ module.exports = {
   'hc2': {documentType: 'hc2', title: 'Health Certificate (HC2) documentation', guidingText: 'Claimants must provide evidence of their Health Certificate (HC2) documentation'},
   'hc3': {documentType: 'hc3', title: 'Health Certificate (HC3) documentation', guidingText: 'Claimants must provide evidence of their Health Certificate (HC3) documentation'},
   'nhs-tax-credit': {documentType: 'nhs-tax-credit', title: 'NHS Tax credit documentation', guidingText: 'Claimants must provide evidence of their NHS Tax credit documentation'},
+  'universal-credit': {documentType: 'universal-credit', title: 'Universal credit documentation', guidingText: 'Claimants must provide evidence of their Universal credit documentation'},
   'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of the claimant\'s tickets.'}
 }
