eb293578ad012411731c6b742f675965bd377922
Michael Hall <m.hall@kainos.com>
2017-03-08 11:22:55 +0000

    Updated test for merge-claim-expenses-with-submitted-responses

--- test/unit/routes/helpers/test-merge-claim-expenses-with-submitted-responses.js
+++ test/unit/routes/helpers/test-merge-claim-expenses-with-submitted-responses.js
@@ -5,8 +5,8 @@ const claimDecisionEnum = require('../../../../app/constants/claim-decision-enum
 var oldData
 var newData
 
-describe('routes/helpers/get-claim-expense-non-persisted-details', function () {
-  before(function (done) {
+describe('routes/helpers/merge-claim-expenses-with-submitted-responses', function () {
+  before(function () {
     oldData = [
       {
         ClaimExpenseId: 1,
@@ -39,36 +39,36 @@ describe('routes/helpers/get-claim-expense-non-persisted-details', function () {
         status: claimDecisionEnum.APPROVED_DIFF_AMOUNT,
         approvedCost: ''
       }]
-    done()
   })
 
-  it('should return an array of claim expenses', function (done) {
-    var nonPersistedExpenseData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
-    expect(nonPersistedExpenseData[0].ClaimExpenseId).to.equal(1)
-    expect(nonPersistedExpenseData[1].ClaimExpenseId).to.equal(2)
-    expect(nonPersistedExpenseData[2].ClaimExpenseId).to.equal(3)
-    done()
+  it('should return an array of claim expenses', function () {
+    var mergedData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
+    expect(mergedData[0].ClaimExpenseId).to.equal(1)
+    expect(mergedData[1].ClaimExpenseId).to.equal(2)
+    expect(mergedData[2].ClaimExpenseId).to.equal(3)
   })
-  it('should set the status of each element in array', function (done) {
-    var nonPersistedExpenseData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
-    expect(nonPersistedExpenseData[0].Status).to.equal(newData[0].status)
-    expect(nonPersistedExpenseData[1].Status).to.equal(newData[1].status)
-    expect(nonPersistedExpenseData[2].Status).to.equal(newData[2].status)
-    done()
+  it('should set the status of each element in array', function () {
+    var mergedData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
+    expect(mergedData[0].Status).to.equal(newData[0].status)
+    expect(mergedData[1].Status).to.equal(newData[1].status)
+    expect(mergedData[2].Status).to.equal(newData[2].status)
   })
-  it('should set the approvedCost of each element in array', function (done) {
-    var nonPersistedExpenseData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
-    expect(nonPersistedExpenseData[0].ApprovedCost).to.equal(newData[0].approvedCost)
-    expect(nonPersistedExpenseData[1].ApprovedCost).to.equal(newData[1].approvedCost)
-    expect(nonPersistedExpenseData[2].ApprovedCost).to.equal(newData[2].approvedCost)
-    done()
+  it('should set the approvedCost of each element in array', function () {
+    var mergedData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
+    expect(mergedData[0].ApprovedCost).to.equal(newData[0].approvedCost)
+    expect(mergedData[1].ApprovedCost).to.equal(newData[1].approvedCost)
+    expect(mergedData[2].ApprovedCost).to.equal(newData[2].approvedCost)
   })
 
-  it('should set Error in only those who have incorrect data', function (done) {
-    var nonPersistedExpenseData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
-    expect(nonPersistedExpenseData[0].Error).to.equal(undefined)
-    expect(nonPersistedExpenseData[1].Error).to.equal(undefined)
-    expect(nonPersistedExpenseData[2].Error).to.equal(true)
-    done()
+  it('should set Error in only those who have incorrect data', function () {
+    var mergedData = mergeClaimExpensesWithSubmittedResponses(oldData, newData)
+    expect(mergedData[0].Error).to.equal(undefined)
+    expect(mergedData[1].Error).to.equal(undefined)
+    expect(mergedData[2].Error).to.equal(true)
+  })
+
+  it('should return the old data if no new data exists', function () {
+    var mergedData = mergeClaimExpensesWithSubmittedResponses(oldData, null)
+    expect(mergedData).to.equal(oldData)
   })
 })
