4d31b533aeaafcf481e4426450e39abc8b4d0ba6
Michael Hall <m.hall@kainos.com>
2017-03-08 15:20:20 +0000

    Updated query for individual claim to modify assignTo if expired

--- app/services/data/get-individual-claim-details.js
+++ app/services/data/get-individual-claim-details.js
@@ -89,6 +89,7 @@ function getClaimantDetails (claimId) {
       'Claim.Status',
       'Claim.PaymentMethod',
       'Claim.AssignedTo',
+      'Claim.AssignmentExpiry',
       'Visitor.FirstName',
       'Visitor.LastName',
       'Visitor.DateOfBirth',
@@ -110,6 +111,12 @@ function getClaimantDetails (claimId) {
       'Prisoner.PrisonNumber',
       'Prisoner.NameOfPrison',
       'Prisoner.NomisCheck')
+    .then(function (data) {
+      if (data.AssignedTo && data.AssignmentExpiry < moment().toDate()) {
+        data.AssignedTo = null
+      }
+      return data
+    })
 }
 
 function getClaimDocuments (claimId, reference, eligibilityId) {
