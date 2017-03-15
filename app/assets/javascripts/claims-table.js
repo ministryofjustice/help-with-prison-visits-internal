984529b628ca2a2284179f878cad8ca69c79368b
Michael Hall <m.hall@kainos.com>
2017-03-15 10:52:17 +0000

    Added error message display to help find error (#312)

--- app/assets/javascripts/claims-table.js
+++ app/assets/javascripts/claims-table.js
@@ -31,8 +31,9 @@ $(document).ready(function () {
       url: dataReference,
       dataSrc: 'claims',
       error: function (response) {
+        var error = JSON.parse(response.responseText)
         $('#claims_processing').hide()
-        alert('An error occurred when retrieving claims.') // eslint-disable-line no-undef
+        alert(error.name + ': ' + error.message) // eslint-disable-line no-undef
       }
     },
 
