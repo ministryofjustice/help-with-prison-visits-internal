24e0afc8dd742a3ebb414cc3601f38ad446be9fe
Michael Hall <m.hall@kainos.com>
2017-03-23 14:49:32 +0000

    Maded requested changes

--- migrations/20170322155201_add-auto-approval-number-of-consecutive-auto-approvals.js
+++ migrations/20170322155201_add-auto-approval-number-of-consecutive-auto-approvals.js
@@ -2,6 +2,12 @@ exports.up = function (knex, Promise) {
   return knex.schema.table('AutoApprovalConfig', function (table) {
     table.integer('NumberOfConsecutiveAutoApprovals')
   })
+  .then(function () {
+    const DEFAULT_NUMBER_OF_CONSECUTIVE_AUTO_APPROVALS = 4
+
+    return knex('AutoApprovalConfig')
+      .update({'NumberOfConsecutiveAutoApprovals': DEFAULT_NUMBER_OF_CONSECUTIVE_AUTO_APPROVALS})
+  })
   .catch(function (error) {
     console.log(error)
     throw error
