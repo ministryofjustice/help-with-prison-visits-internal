bd6c77c091e05ec590f39bd71737ff6559a10c59
Michael Hall <m.hall@kainos.com>
2017-03-29 09:20:47 +0100

    Update date helper to not add hour when after DST as already added (#323)

--- app/views/helpers/date-helper.js
+++ app/views/helpers/date-helper.js
@@ -1,13 +1,13 @@
 const moment = require('moment')
 
 module.exports.shortDate = function (date) {
-  return moment(date).format('DD/MM/YYYY')
+  return moment.utc(date).format('DD/MM/YYYY')
 }
 
 module.exports.longDate = function (date) {
-  return moment(date).format('Do MMMM YYYY')
+  return moment.utc(date).format('Do MMMM YYYY')
 }
 
 module.exports.shortDateAndTime = function (date) {
-  return moment(date).format('DD/MM/YY HH:mm')
+  return moment.utc(date).format('DD/MM/YY HH:mm')
 }
