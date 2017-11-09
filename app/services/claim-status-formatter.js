e617ee896c3b4899d99e9fc74e1037f6aeb5d03b
kevinfox <kevinfox@kainos.com>
2017-11-09 11:06:47 +0000

    Fixed indentation and removed semi-colons and log statements

--- app/services/claim-status-formatter.js
+++ app/services/claim-status-formatter.js
@@ -1,30 +1,28 @@
 module.exports = function (status) {
-    var displayStatus = ''
-    status = status.trim()
-    switch (status) {
-        case 'APPROVED':
-        case 'AUTO-APPROVED':
-        case 'REJECTED':
-        case 'UPDATED':
-        case 'PENDING':
-        case 'NEW':
-            console.log('In here')
-            displayStatus = status.charAt(0) + status.slice(1).toLowerCase()
-            break;
-        case 'APPROVED-ADVANCE-CLOSED':
-            displayStatus = 'Closed'
-            break;
-        case 'APPROVED-PAYOUT-BARCODE-EX':
-            displayStatus = 'Barcode Expired'
-            break;
-        case 'REQUEST-INFO-PAYMENT':
-            displayStatus = 'Payment Information Requested'
-            break;
-        case 'REQUEST-INFORMATION':
-            displayStatus = 'Information Requested'
-            break;
-        default:
-            displayStatus = status;
-    }
-    return displayStatus
+	var displayStatus = ''
+	switch (status) {
+		case 'APPROVED':
+		case 'AUTO-APPROVED':
+		case 'REJECTED':
+		case 'UPDATED':
+		case 'PENDING':
+		case 'NEW':
+			displayStatus = status.charAt(0) + status.slice(1).toLowerCase()
+			break
+		case 'APPROVED-ADVANCE-CLOSED':
+			displayStatus = 'Closed'
+			break
+		case 'APPROVED-PAYOUT-BARCODE-EX':
+			displayStatus = 'Barcode Expired'
+			break
+		case 'REQUEST-INFO-PAYMENT':
+			displayStatus = 'Payment Information Requested'
+			break
+		case 'REQUEST-INFORMATION':
+			displayStatus = 'Information Requested'
+			break
+		default:
+			displayStatus = status
+	}
+	return displayStatus
 }
