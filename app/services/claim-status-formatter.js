e0b3f9c8be51b2adb8083b00557241ab9038c39b
kevinfox <kevinfox@kainos.com>
2017-11-09 11:15:40 +0000

    Changed tabs to spaces and inserted new line at end of file

--- app/services/claim-status-formatter.js
+++ app/services/claim-status-formatter.js
@@ -1,28 +1,29 @@
 module.exports = function (status) {
-	var displayStatus = ''
-	switch (status) {
-		case 'APPROVED':
-		case 'AUTO-APPROVED':
-		case 'REJECTED':
-		case 'UPDATED':
-		case 'PENDING':
-		case 'NEW':
-			displayStatus = status.charAt(0) + status.slice(1).toLowerCase()
-			break
-		case 'APPROVED-ADVANCE-CLOSED':
-			displayStatus = 'Closed'
-			break
-		case 'APPROVED-PAYOUT-BARCODE-EX':
-			displayStatus = 'Barcode Expired'
-			break
-		case 'REQUEST-INFO-PAYMENT':
-			displayStatus = 'Payment Information Requested'
-			break
-		case 'REQUEST-INFORMATION':
-			displayStatus = 'Information Requested'
-			break
-		default:
-			displayStatus = status
-	}
-	return displayStatus
+  var displayStatus = ''
+  switch (status) {
+    case 'APPROVED':
+    case 'AUTO-APPROVED':
+    case 'REJECTED':
+    case 'UPDATED':
+    case 'PENDING':
+    case 'NEW':
+      displayStatus = status.charAt(0) + status.slice(1).toLowerCase()
+      break
+    case 'APPROVED-ADVANCE-CLOSED':
+      displayStatus = 'Closed'
+      break
+    case 'APPROVED-PAYOUT-BARCODE-EX':
+      displayStatus = 'Barcode Expired'
+      break
+    case 'REQUEST-INFO-PAYMENT':
+      displayStatus = 'Payment Information Requested'
+      break
+    case 'REQUEST-INFORMATION':
+      displayStatus = 'Information Requested'
+      break
+    default:
+      displayStatus = status
+      break
+  }
+  return displayStatus
 }
