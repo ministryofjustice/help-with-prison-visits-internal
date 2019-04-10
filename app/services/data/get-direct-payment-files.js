4647cafe3884d5dfcae907095150a0d974586eab
kevinfox <kevinfox@kainos.com>
2019-04-10 14:10:25 +0100

    APVS0016: Adding advanced search on payment method. Adding extra column for APVU/SSCL Bank payment file

--- app/services/data/get-direct-payment-files.js
+++ app/services/data/get-direct-payment-files.js
@@ -6,16 +6,19 @@ const Promise = require('bluebird')
 module.exports = function () {
   const NUMBER_OF_PAYMENT_FILES = parseInt(config.PAYMENT_NUMBER_OF_PAYMENT_FILES)
   const ACCESSPAY_FILE = 'ACCESSPAY_FILE'
+  const APVU_ACCESSPAY_FILE = 'APVU_ACCESSPAY_FILE'
   const ADI_JOURNAL_FILE = 'ADI_JOURNAL_FILE'
 
   return Promise.all([
     getPaymentFiles(ACCESSPAY_FILE, NUMBER_OF_PAYMENT_FILES),
-    getPaymentFiles(ADI_JOURNAL_FILE, NUMBER_OF_PAYMENT_FILES)
+    getPaymentFiles(ADI_JOURNAL_FILE, NUMBER_OF_PAYMENT_FILES),
+    getPaymentFiles(APVU_ACCESSPAY_FILE, NUMBER_OF_PAYMENT_FILES),
   ])
   .then(function (results) {
     return {
       accessPayFiles: results[0],
-      adiJournalFiles: results[1]
+      adiJournalFiles: results[1],
+      apvuAccessPayFiles: results[2]
     }
   })
 }
