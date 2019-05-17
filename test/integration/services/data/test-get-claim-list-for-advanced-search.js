49b83990d2819467b2594fc70a36e1a70e0c0873
kevinfox <kevinfox@kainos.com>
2019-05-17 10:45:34 +0100

    APVS0195: Fixing coding standards

--- test/integration/services/data/test-get-claim-list-for-advanced-search.js
+++ test/integration/services/data/test-get-claim-list-for-advanced-search.js
@@ -82,7 +82,7 @@ describe('services/data/get-claim-list-for-advanced-search', function () {
         expect(result.claims[0].Reference, `Reference should equal ${reference1}`).to.equal(reference1)
         expect(result.claims[0].ClaimId, `ClaimId should equal ${claimId}`).to.equal(claimId)
         expect(result.claims.length, 'Claims length should equal 1').to.equal(1)
-        expect(result.claims[0].DaysUntilPayment,'Days Until Payment should equal 5').to.equal(5)
+        expect(result.claims[0].DaysUntilPayment, 'Days Until Payment should equal 5').to.equal(5)
       })
   })
 
@@ -93,7 +93,7 @@ describe('services/data/get-claim-list-for-advanced-search', function () {
 
     return getClaimListForAdvancedSearch(searchCriteria, 0, 10)
       .then(function (result) {
-        expect(result.claims[0].DaysUntilPayment,'Days Until Payment should equal N/A').to.equal('N/A')
+        expect(result.claims[0].DaysUntilPayment, 'Days Until Payment should equal N/A').to.equal('N/A')
       })
   })
 
