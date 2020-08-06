53352fadea50d7807170be8923cd8b397626b64c
kevinfox <kevinfox@kainos.com>
2020-08-06 11:10:29 +0100

    APVS0203: Now only requiring receipts for parking not fuel

--- app/constants/receipt-and-processed-manually-enum.js
+++ app/constants/receipt-and-processed-manually-enum.js
@@ -1,57 +1,46 @@
 module.exports = {
   car: {
     receiptRequired: false,
-    receiptOptional: true,
     processedManually: false
   },
   toll: {
     receiptRequired: false,
-    receiptOptional: false,
     processedManually: false
   },
-  'parking': {
+  parking: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   hire: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   bus: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   train: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: true
   },
   taxi: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   plane: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   ferry: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   },
   refreshment: {
     receiptRequired: false,
-    receiptOptional: false,
     processedManually: false
   },
   accommodation: {
     receiptRequired: true,
-    receiptOptional: false,
     processedManually: false
   }
 }
