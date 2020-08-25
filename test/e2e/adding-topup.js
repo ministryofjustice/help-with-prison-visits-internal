48fc60054f1591945375ee754e40824776e19e0d
kevinfox <kevinfox@kainos.com>
2020-08-25 15:39:18 +0100

    APVS0194: Fixing top up end-to-end test

--- test/e2e/adding-topup.js
+++ test/e2e/adding-topup.js
@@ -53,7 +53,7 @@ describe('Adding a new top up flow', () => {
     var submitButton = await $('#add-top-up')
     await submitButton.click()
 
-    await browser.pause(10000)
+    await browser.pause(3000)
 
     var claimDetails = await getIndividualClaimDetails(claimId)
     expect(claimDetails.TopUps.length, 'TopUps should be an array of length 1').to.equal(1)
