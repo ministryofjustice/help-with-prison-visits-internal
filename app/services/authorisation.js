7fad697f8c98352fd10dc35251a40ee86d9d987b
kevinfox <kevinfox@kainos.com>
2021-03-16 10:50:05 +0000

    APVS0246: Adding new authentication method. Instead of a function for each role, we will now have one function and pass the roles that are allowed as a parameter

--- app/services/authorisation.js
+++ app/services/authorisation.js
@@ -1,7 +1,25 @@
+// APVS0246
 function isAuthenticated (req) {
   if (!req.user) {
     const error = new Error('unauthenticated')
     error.status = 401
+    throw error
+  }
+}
+
+function hasRoles (req, roles) {
+  isAuthenticated(req)
+
+  let hasDesiredRole = false
+  roles.forEach(function (role) {
+    if (req.user.roles.includes(role)) {
+      hasDesiredRole = true
+      return
+    }
+  })
+  if (!hasDesiredRole) {
+    const error = new Error('unauthorised')
+    error.status = 403
     throw error
   }
 }
@@ -40,3 +58,4 @@ module.exports.isAuthenticated = isAuthenticated
 module.exports.isAdmin = isAdmin
 module.exports.isSscl = isSscl
 module.exports.isCaseworker = isCaseworker
+module.exports.hasRoles = hasRoles
