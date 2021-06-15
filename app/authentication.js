fbaac3c5738de5a855ea8c1b803ef29c81936bb2
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-06-15 08:47:38 +0100

    Remove axios to check auth

--- app/authentication.js
+++ app/authentication.js
@@ -3,7 +3,7 @@ const session = require('express-session')
 const redis = require('redis')
 const passport = require('passport')
 const OAuth2Strategy = require('passport-oauth2').Strategy
-const axios = require('axios')
+const request = require('request')
 const RedisStore = require('connect-redis')(session)
 const log = require('./services/log')
 const applicationRoles = require('./constants/application-roles-enum')
@@ -59,21 +59,21 @@ module.exports = function (app) {
         Authorization: `Basic ${Buffer.from(`${config.CLIENT_ID}:${config.CLIENT_SECRET}`).toString('base64')}`
       }
     },
-    function (accessToken, refreshToken, params, profile, cb) {
+    function (accessToken, refreshToken, params, profile, done) {
       // Call API to get details on user
       const options = {
         uri: `${config.TOKEN_HOST}${config.USER_PATH_PREFIX}${config.USER_DETAILS_PATH}`,
         qs: { access_token: accessToken },
         json: true
       }
-      axios(options, function (error, response, userDetails) {
+      request(options, function (error, response, userDetails) {
         if (!error && response.statusCode === 200) {
           let roles = []
           options.uri = `${config.TOKEN_HOST}${config.USER_PATH_PREFIX}/${userDetails.username}${config.USER_EMAIL_PATH}`
-          axios(options, function (error, response, userEmail) {
+          request(options, function (error, response, userEmail) {
             if (!error && response.statusCode === 200) {
               options.uri = `${config.TOKEN_HOST}${config.USER_PATH_PREFIX}${config.USER_ROLES_PATH}`
-              axios(options, function (error, response, userRoles) {
+              request(options, function (error, response, userRoles) {
                 if (!error && response.statusCode === 200) {
                   userRoles.forEach(function (role) {
                     roles = roles.concat(role.roleCode)
@@ -85,26 +85,26 @@ module.exports = function (app) {
                       name: userDetails.name,
                       roles: roles
                     }
-                    cb(null, sessionUser)
+                    done(null, sessionUser)
                   } else {
                     log.error('no roles found for user')
                     const notAuthorisedError = new Error('You are not authorised for this service')
                     notAuthorisedError.status = 403
-                    cb(notAuthorisedError, null)
+                    done(notAuthorisedError, null)
                   }
                 } else {
                   log.error({ error: error }, 'error returned when requesting user roles from SSO')
-                  cb(error, null)
+                  done(error, null)
                 }
               })
             } else {
               log.error({ error: error }, 'error returned when requesting user email from SSO')
-              cb(error, null)
+              done(error, null)
             }
           })
         } else {
           log.error({ error: error }, 'error returned when requesting user details from SSO')
-          cb(error, null)
+          done(error, null)
         }
       })
     }))
