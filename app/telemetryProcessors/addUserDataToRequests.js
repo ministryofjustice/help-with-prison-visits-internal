4bf6a4c5323a3f5227c9ac86b2474ab9503f0d3c
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-08-18 09:55:23 +0100

    Add more debugging

--- app/telemetryProcessors/addUserDataToRequests.js
+++ app/telemetryProcessors/addUserDataToRequests.js
@@ -2,8 +2,11 @@ const appInsights = require('applicationinsights')
 
 module.exports = function addUserDataToRequests (envelope, contextObjects) {
   const isRequest = envelope.data.baseType === appInsights.Contracts.TelemetryTypeString.Request
+  console.log(`isRequest:${isRequest}`)
   if (isRequest) {
-    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.req?.user || {}
+    console.log(JSON.stringify(contextObjects))
+    console.log(JSON.stringify(contextObjects?.['http.ServerRequest']?.req))
     if (username) {
       const { properties } = envelope.data.baseData
       // eslint-disable-next-line no-param-reassign
