9ccd667931c4c1533ddec757162d4b224ab4e803
Ash <96007411+ashleygyngell@users.noreply.github.com>
2024-07-08 17:36:16 +0100

    Updating errors components & layout tweak to accomodate (#651)

--- app/services/nunjucks-setup.js
+++ app/services/nunjucks-setup.js
@@ -21,4 +21,34 @@ module.exports = function (app, developmentMode) {
   njkEnv.addFilter('concat', function (arr1, arr2) {
     return arr1.concat(arr2)
   })
+
+  const getMojFilters = require('@ministryofjustice/frontend/moj/filters/all')
+
+  const mojFilters = getMojFilters()
+  Object.keys(mojFilters).forEach(filterName => {
+    njkEnv.addFilter(filterName, mojFilters[filterName])
+  })
+
+  // convert errors to format for GOV.UK error summary component
+  njkEnv.addFilter('errorSummaryList', (errors = []) => {
+    return Object.keys(errors).map((error) => {
+      const errorListItem = {}
+      errorListItem.text = errors[error][0]
+      if (error !== 'expired') {
+        errorListItem.href = `#${error}`
+      }
+      return errorListItem
+    })
+  })
+
+  // find specifc error and return errorMessage for field validation
+  njkEnv.addFilter('findError', (errors, formFieldId) => {
+    if (!errors || !formFieldId) return null
+    if (errors[formFieldId]) {
+      return {
+        text: errors[formFieldId][0]
+      }
+    }
+    return null
+  })
 }
